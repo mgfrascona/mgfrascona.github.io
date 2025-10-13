20ae1ec128fa37aa0474ef81782dc2b741d7fe3c
mgfrascona <michaelgfrascona@gmail.com>
2025-10-13 14:56:58 -0500

    Added carousel

--- scripts/site.js
+++ scripts/site.js
@@ -1,2 +1,41 @@
 const key = "It's a secret to everybody."
 localStorage.setItem(key, JSON.stringify('Guess what? Chicken butt.'))
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
+prevButton.addEventListener('click', () => {
+    currentImage = (currentImage - 1 + urls.length) % urls.length;
+    showImages();
+});
+
+nextButton.addEventListener('click', () => {
+    currentImage = (currentImage + 1) % urls.length;
+    showImages();
+});
+
+setInterval(() => {
+    currentImage = (currentImage + 1) % urls.length;
+    showImages();
+}, 3000)
